f25e559db3d990bc872d4e6455a0c063f0be58f4
Timothy <31729676+kolesnik-tim@users.noreply.github.com>
2018-06-10 22:38:33 +0300

    fix

--- src/js/import/slider.js
+++ src/js/import/slider.js
@@ -1,3 +1,11 @@
 import Swiper from 'swiper';
 import owl from 'owl.carousel';
 import owlThumbs from 'owl.carousel2.thumbs';
+
+
+var swiper = new Swiper('.swiper-intro', {
+  navigation: {
+    nextEl: '.swiper-intro-right',
+    prevEl: '.swiper-intro-left',
+  },
+});
