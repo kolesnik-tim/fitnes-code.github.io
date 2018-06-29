b00b236722381088ec9518f4c4e3810921e7e0db
Timothy <31729676+kolesnik-tim@users.noreply.github.com>
2018-06-29 17:39:44 +0300

    fix

--- src/js/import/ui.js
+++ src/js/import/ui.js
@@ -9,6 +9,40 @@ new WOW().init();
 
 //select
 $('select').selectize('options');
+if($(window).width() <= 767) {
+  var didScroll;
+  var lastScrollTop = 0;
+  var delta = 5;
+  var navbarHeight = $('.header').outerHeight();
+
+  $(window).scroll(function(event) {
+    didScroll = true;
+  });
+
+  setInterval(function() {
+    if (didScroll) {
+      hasScrolled();
+      didScroll = false;
+    }
+  }, 250);
+
+  function hasScrolled() {
+    var st = $(window).scrollTop();
+    if(Math.abs(lastScrollTop - st) <= delta)
+      return;
+    if (st > lastScrollTop && st > navbarHeight) {
+    // Scroll Down
+      $('header').addClass('scroll');
+    } else {
+    // Scroll Up
+      if(st + $(window).height() < $(document).height()) {
+        $('header').removeClass('scroll');
+      }
+    }
+    
+    lastScrollTop = st;
+  }
+}
 
 
 
@@ -17,16 +51,6 @@ $('.header__services p').on('click', function() {
   $('.header__services__option').slideToggle();
   $(this).toggleClass('active');
 });
-
-if($(window).width() <= 767) {
-  $(window).scroll(function() {
-    if($(window).scrollTop() > 60) {
-      $('.header').addClass('scroll');
-    } else{
-      $('.header').removeClass('scroll');
-    }
-  });
-}
 
 
 //menu open
